369e6d72e942b62c2bf210ffceef3eaea1261ab4
VongCanhChi <64590894+VongCanhChi@users.noreply.github.com>
2020-05-01 09:11:55 +0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -11,27 +11,6 @@ int main()
 
 	ScanQInt(x, str);
 
-
-//	int daybit[128];
-//	qIntToDayBit(x, daybit);
-//	for (int i = 0; i < 128; i++)
-//{
-//	cout << daybit[i];
-//}
-
-	//cout << endl;
-	//int dayso1[100];
-	//tinhHaiMuX(dayso1, 12);
-	//int dayso2[100];
-	//tinhHaiMuX(dayso2, 12);
-	//
-	//truHaiDayBit(dayso1, dayso2);
-	//for (int i = 0; i < 100; i++)
-	//{
-	//	cout << dayso2[i];
-	//}
-
-
 	cout << endl;
 	PrintQInt(x);
 
